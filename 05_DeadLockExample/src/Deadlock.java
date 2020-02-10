0db3502c8677fd52e6f5e8844086823cde089976
Anwar Mamat <anwarmamat@gmail.com>
2020-02-10 10:41:32 -0500

    adds delays

--- 05_DeadLockExample/src/Deadlock.java
+++ 05_DeadLockExample/src/Deadlock.java
@@ -11,10 +11,11 @@ public class Deadlock {
 			System.out
 					.println(count + ":\t" + "I, " + this.name + " bow to you, " + bower.name);
 			count++;
-			bower.bowBack(this);
+			bower.bowBack(this);			
 		}
 
 		public synchronized void bowBack(Friend bower) {
+			
 			System.out.println(count +":\t" + "I, " + this.name + " bow back to you,"
 					+ bower.name);
 			
@@ -22,20 +23,41 @@ public class Deadlock {
 	}
 
 	public static void main(String[] args) {
+		
+		
 		final Friend f1 = new Friend("Alphonse"), f2 = new Friend("Gaston");
-
+		final int delay = 100;
+		
 		(new Thread(new Runnable() {
 			public void run() {
 				while (true) {
 					f1.bow(f2);
+					
+					try {
+						Thread.sleep((int)(Math.random() * delay));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		})).start();
 		
 		(new Thread(new Runnable() {
 			public void run() {
+				try {
+					Thread.sleep((int)(Math.random() * delay));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				while (true) {
+					
 					f2.bow(f1);
+					
+					try {
+						Thread.sleep((int)(Math.random() * delay));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		})).start();
