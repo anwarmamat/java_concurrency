8cb6b2cfc917e878c5ff7e51c22a3173705023ac
Anwar Mamat <anwarmamat@gmail.com>
2021-11-04 08:20:40 -0400

    adds mmore mpi examples

--- 22-mpi/hello.c
+++ 22-mpi/hello.c
@@ -1,28 +1,24 @@
-#include <mpi.h>
+
+
+#include "mpi.h"
 #include <stdio.h>
+#include <stdlib.h>
+#define  MASTER 0
 
-int main(int argc, char** argv) {
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
+int main (int argc, char *argv[])
+{
+  int   numtasks, rank, len;
+  char hostname[MPI_MAX_PROCESSOR_NAME];
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Init(&argc, &argv);
 
-    // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  MPI_Get_processor_name(hostname, &len);
+  printf ("Hello from task %d on %s!\n", rank, hostname);
+  if (rank == MASTER)
+    printf("MASTER: Number of MPI tasks is: %d\n",numtasks);
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+  MPI_Finalize();
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d"
-           " out of %d processors\n",
-           processor_name, world_rank, world_size);
-
-    // Finalize the MPI environment.
-    MPI_Finalize();
 }
